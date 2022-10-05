b7d15f4abe8d1287fdad353cce762d2cb4253c1a
perrynzhou <perrynzhou@gmail.com>
2022-10-05 10:43:51 +0000

    updates

--- management/src/mgmt_cmd.h
+++ management/src/mgmt_cmd.h
@@ -11,14 +11,20 @@
 typedef enum {
    MGMT_DEL_NODE_CMD=0,
    MGMT_ADD_NODE_CMD,
+   MGMT_MNT_CMD,
 }mgmt_cmd_type;
 
+typedef enum {
+   MGMT_META_NODE=0,
+   MGMT_DATA_NODE,
+}mgmt_node_type;
 typedef struct  {
    size_t  len;
-   mgmt_cmd_type type;
+   mgmt_cmd_type cmd_type;
+   mgmt_node_type  node_type;
   char service_node_addr[0];
 }mgmt_request;
 
-mgmt_request *mgmt_request_alloc(char *service_addr,mgmt_cmd_type type);
+mgmt_request *mgmt_request_alloc(char *service_addr, mgmt_node_type  node_type,mgmt_cmd_type cmd_type);
 void mgmt_request_destroy(mgmt_request *req);
 #endif
